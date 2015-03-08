a176ddc76f2c28b99044df8b7eb0c26d06230690
Asheesh Laroia <asheesh@asheesh.org>
2015-03-08 15:57:19 -0700

    Provide a made-up MySQL username+password

--- index.php
+++ index.php
@@ -16,15 +16,6 @@
     <!-- Custom styles for this template -->
     <link href="jumbotron.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
@@ -47,8 +38,9 @@
     <div class="jumbotron">
       <div class="container">
         <h1>Hi! I'm PHP!</h1>
-        <p>If you look below, you'll see how different bits of the Sandstorm platform can be used from PHP. You can view the PHP source online of this app, too.</p>
-        <p><a class="btn btn-primary btn-lg" href="https://github.com/paulproteus/php-app-to-package-for-sandstorm" role="button">View the source &raquo;</a></p>
+        <p>This demo app showcases some normal PHP functionality when running in Sandstorm. This app was packaged with <tt>vagrant-spk</tt>, which you can use for your app too.</p>
+        <p><a class="btn btn-primary btn-lg" href="https://github.com/paulproteus/php-app-to-package-for-sandstorm" role="button">View this app's source &raquo;</a> <a class="btn btn-default btn-lg" href="https://github.com/sandstorm-io/sandstorm/wiki/User-Authentication" role="button">View vagrant-spk docs &raquo;</a></p>
+</p>
       </div>
     </div>
 
@@ -66,19 +58,55 @@
           <p><a class="btn btn-default" href="https://github.com/sandstorm-io/sandstorm/wiki/User-Authentication" role="button">View user authentication docs &raquo;</a></p>
         </div>
         <div class="col-md-4">
-          <h2>Current URL information</h2>
-          <p>FIXME Sandstorm Platform Something.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
           <h2>Writable data in /var</h2>
           <p>When this app starts, it creates a file called <tt>/var/number.txt</tt>.</p>
+          <!-- Make the file if necessary. -->
           <?php if ( ! file_exists("/var/number.txt") ) { $fp = open('/var/number.txt', 'w'); fwrite($fp, "0"); fclose($fp); } ?>
+          <!-- Increment it! This is a page view! -->
+          <?php $fp = open('/var/number.txt', 'r'); $s = fread($fp, 1000); $s = intval($s) + 1; fclose($fp);
+                $fp = open('/var/number.txt', 'w'); fwrite($fp, $s); fclose($fp);
+                ?>
+          <!-- Now print it out, using the simplistic and convenient PHP readfile(). -->
           <p>Current contents of the file: <?php echo readfile("/var/number.txt"); ?></p>
-          <p>FIXME make a button to click to increment it by 1.</p>
+          <p>Every time you reload this page within an instance of the app, the number goes up.</p>
         </div>
+        <div class="col-md-4">
+          <h2>Fresh instances</h2>
+          <p>Feel free to make a new instance of this app from the Sandstorm shell!</p>
+          <p>Each new instance will run with a fresh MySQL and <tt>/var</tt>.</p>
+       </div>
       </div>
 
+      <div class="row">
+        <div class="col-md-4">
+          <h2>External network access</h2>
+          <p>By default, the app has no network access. That means the following will fail:</p>
+          <p>FIXME php_curl() something.</p>
+        </div>
+        <div class="col-md-4">
+          <h2>Outbound email</h2>
+          <p>Mumble.</p>
+        </div>
+        <div class="col-md-4">
+          <h2>MySQL</h2>
+          <p><tt>vagrant-spk</tt> sets up one MySQL instance per app
+          instance. This means you can hard-code a MySQL database name
+          and password and it'll work on every instance.</p>
+          <p>Here's the output of <tt>SELECT 1+1;</tt></p>
+          <?php
+             $db = mysqli_connect("localhost", "root", "", "") or die("Error " . mysqli_error($db));
+             $result = mysqli_query($db , "SELECT 1+1;");
+             ?>
+          <p><tt><?php echo $result; ?></tt></p>
+          <p>Every app instance can use MySQL with this information:</p>
+          <ul>
+            <li>Host: <tt>localhost</tt> (not 127.0.0.1)</li>
+            <li>Username: <tt>root</tt></li>
+            <li>Password: <tt>funtime</tt></li>
+            <li>Database: Whatever you create. See the docs.</li>
+          </ul>
+        </div>
+      </div>
       <hr>
 
       <footer>
